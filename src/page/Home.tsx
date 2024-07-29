cae9a03359c3ddf323b9f56f5699d258176f6bda
heajun jeong <heajun35@gmail.com>
2024-07-29 16:28:58 +0900

    [Feat] - Layout 작성

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main>
+      <Link to={"/sign"}>로그인/회원가입</Link>
+      <Link to={"/my-page"}>로그인/회원가입</Link>
+    </main>
+  );
 };
 
 export default Home;
--- src/share/Router.tsx
+++ src/share/Router.tsx
@@ -1,17 +1,26 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import NonAuthLayout from "../page/layout/NonAuthLayout";
+import AuthLayout from "../page/layout/AuthLayout";
 import Home from "../page/Home";
 import Sign from "../page/Sign";
 import Test from "../page/Test";
+import MyPage from "../page/MyPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/sign" element={<Sign />} />
-        <Route path="/my-page" element={<Home />} />
         <Route path="/test" element={<Test />} />
+
+        <Route element={<NonAuthLayout />}>
+          <Route path="/sign" element={<Sign />} />
+        </Route>
+
+        <Route element={<AuthLayout />}>
+          <Route path="/my-page" element={<MyPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
