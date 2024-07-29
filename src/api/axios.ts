61a3006ca71ec712c94815169bab9edc81268892
heajun jeong <heajun35@gmail.com>
2024-07-29 16:50:58 +0900

    [Feat] - jwt instance 작성

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,3 +3,7 @@ import axios from "axios";
 export const jsonInstance = axios.create({
   baseURL: import.meta.env.VITE_SOME_TEST_URL,
 });
+
+export const jwtInstance = axios.create({
+  baseURL: import.meta.env.VITE_SOME_SIGN_URL,
+});
