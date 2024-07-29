87166eb0b3fd89fd8660e3f4cd678dca8661e64d
heajun jeong <heajun35@gmail.com>
2024-07-29 16:08:56 +0900

    [Feat] - todoList 작성

--- src/page/Test.tsx
+++ src/page/Test.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useJsonPlaceHolderTodosQuery } from "../hook/useQuery";
 
 import type { Todo } from "../types";
@@ -14,16 +15,29 @@ const Test = () => {
   }
 
   return (
-    <main>
-      {data.map((item: Todo) => {
-        return (
-          <ol className="flex border border-solid border-black" key={item.id}>
-            <h2>{item.title}</h2>
-            <p>{item.userId}</p>
-          </ol>
-        );
-      })}
-    </main>
+    <>
+      <Link
+        className="sticky top-2 left-2 p-2 border-2 border-solid border-black rounded-lg"
+        to={"/"}
+      >
+        홈으로
+      </Link>
+      <main className="flex flex-col items-center">
+        <h1 className="text-4xl my-3 font-bold">TodoList</h1>
+
+        {data.map((item: Todo) => {
+          return (
+            <ol
+              className="flex items-center my-1 px-2 w-min h-10 text-nowrap border border-solid border-black rounded-lg"
+              key={item.id}
+            >
+              <h2>{item.title}</h2>
+              <p>{item.userId}</p>
+            </ol>
+          );
+        })}
+      </main>
+    </>
   );
 };
 
