3159f553e98dd09f82c9ef9088d8fb3f993cb1ba
heajun jeong <heajun35@gmail.com>
2024-07-30 17:00:35 +0900

    [Feat] - jwt api 작성

--- src/api/jwt.ts
+++ src/api/jwt.ts
@@ -1,10 +1,41 @@
 import { jwtInstance } from "./axios";
 
-import type { UserInfoInput } from "../types";
+import type { Profile, UserInfoInput } from "../types";
 
 export const signUp = async (userInfo: UserInfoInput) => {
   try {
     const res = await jwtInstance.post("/register", userInfo);
+    return res.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const signIn = async (userInfo: Omit<UserInfoInput, "nickname">) => {
+  try {
+    const res = await jwtInstance.post("/login", userInfo);
+    return res.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const userCheck = async (token: string) => {
+  try {
+    const res = await jwtInstance.get("/user", {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return res.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const profileChange = async (token: string, profileInput: Profile) => {
+  try {
+    const res = await jwtInstance.patch("/profile", profileInput, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
     return res.data;
   } catch (error) {
     return error;
--- src/types/index.ts
+++ src/types/index.ts
@@ -10,3 +10,8 @@ export type UserInfoInput = {
   password: string;
   nickname: string;
 };
+
+export type Profile = {
+  avatar: File;
+  nickname: string;
+};
