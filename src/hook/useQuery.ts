2eaca964d91ba8f203dfdf69d338b5082338b4b0
heajun jeong <heajun35@gmail.com>
2024-07-30 17:40:57 +0900

    [Feat] - userCheck query 작성

--- src/hook/useQuery.ts
+++ src/hook/useQuery.ts
@@ -3,6 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getTodo, getTodos } from "../api/jsonPlaceHolder";
 
 import QUERY_KEY from "../util/queryKey";
+import { userCheck } from "../api/jwt";
 
 export const useJsonPlaceHolderTodosQuery = () => {
   const { isFetching, isError, data } = useQuery({
@@ -11,6 +12,7 @@ export const useJsonPlaceHolderTodosQuery = () => {
     staleTime: 1000 * 3,
     gcTime: 1000 * 10,
     refetchOnWindowFocus: false,
+    retry: 0,
   });
 
   return { isFetching, isError, data };
@@ -23,6 +25,18 @@ export const useJsonPlaceHolderTodoQuery = () => {
     staleTime: 1000 * 3,
     gcTime: 1000 * 10,
     refetchOnWindowFocus: false,
+    retry: 0,
+  });
+
+  return { isFetching, isError, data };
+};
+
+export const useUserCheckQuery = () => {
+  const { isFetching, isError, data } = useQuery({
+    queryKey: [QUERY_KEY.userCheck],
+    queryFn: () => userCheck(localStorage.getItem("token")!),
+    refetchOnWindowFocus: false,
+    retry: 0,
   });
 
   return { isFetching, isError, data };
