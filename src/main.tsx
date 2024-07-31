4f08cb768348530b45fad8624e9a6bb9f9bd66ef
heajun jeong <heajun35@gmail.com>
2024-07-31 19:42:26 +0900

    [Feat] - sentry setting

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
-  dsn: import.meta.env.VITE_SOME_SENTRY_AUTH_TOKEN,
+  dsn: "https://d92ece81b019f6253a112545e6a28e8b@o4507695313715200.ingest.us.sentry.io/4507695315877888",
   integrations: [
     Sentry.browserTracingIntegration({}),
     Sentry.browserProfilingIntegration(),
