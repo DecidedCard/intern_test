b3998cdf5f9b77e103a86994678be92b8118731b
heajun jeong <heajun35@gmail.com>
2024-07-31 18:49:24 +0900

    [Feat] - sentry 테스트용 버튼 제거

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -5,16 +5,7 @@ const Home = () => {
     <main className="flex items-center justify-center gap-5 mx-auto h-96">
       <Navigation text="로그인/회원가입" path="/sign" />
       <Navigation text="마이페이지" path="/my-page" />
-      <Navigation text="JSONplaceHolder test" path="/test" />
-      <button
-        type="button"
-        onClick={() => {
-          throw new Error("Sentry Test Error");
-        }}
-      >
-        Break the world
-      </button>
-      ;
+      <Navigation text="JSONplaceHolder test" path="/test" />;
     </main>
   );
 };
