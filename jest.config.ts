dc3b7630d0feace8e2de5e46346eeab10bb09160
heajun jeong <heajun35@gmail.com>
2024-07-31 14:33:01 +0900

    [Feat] - jest setting 에러 발생 고치는 중

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,17 @@
 export default {
   testEnvironment: "jsdom",
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.(ts|tsx)$": "ts-jest",
   },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   moduleNameMapper: {
     "^.+\\.svg$": "jest-svg-transformer",
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.app.json",
+    },
+  },
 };
